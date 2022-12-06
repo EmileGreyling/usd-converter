7e13e5e14324836598d20e2116abd7caf903652c
Emile Greyling <coding-hacks@localhost.localdomain>
2022-12-06 11:02:33 +0200

    Added an extra currency

--- main.js
+++ main.js
@@ -5,4 +5,6 @@ document.querySelector('#randInput').addEventListener('input', function (e) {
     document.querySelector('#randOutput').innerHTML = `R${usd * 17.46}`;
     document.querySelector('#eurOutput').innerHTML = `€${usd * 0.95}`;
     document.querySelector('#cadOutput').innerHTML = `C$${usd * 1.36}`;
+    document.querySelector('#audOutput').innerHTML = `A$${usd * 1.49}`;
+
 });
