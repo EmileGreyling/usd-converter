36c71b3d5d0abb6f5f1ec9c07c21fe00aa160fed
Emile Greyling <coding-hacks@localhost.localdomain>
2022-12-06 09:25:37 +0200

    Added JavaScript Code

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('output').style.visibility = 'hidden';
+document.querySelector('#randInput').addEventListener('input', function (e) {
+    document.getElementById('output').style.visibility = 'visible';
+    let usd = e.target.value;
+    document.querySelector('#randOutput').innerHTML = `R${usd * 17.46}`;
+    document.querySelector('#eurOutput').innerHTML = `€${usd * 0.95}`;
+    document.querySelector('#cadOutput').innerHTML = `C$${usd * 1.36}`;
+});
